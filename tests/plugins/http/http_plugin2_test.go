548aa7d167eb9e4638af322b855fabb8aee9cc7b
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-10-27 13:31:11 +0300

    Fix SSL panic
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- tests/plugins/http/http_plugin2_test.go
+++ tests/plugins/http/http_plugin2_test.go
@@ -129,3 +129,72 @@ func echoHTTPPost(t *testing.T) {
 		_ = resp.Body.Close()
 	}
 }
+
+func TestSSLNoHTTP(t *testing.T) {
+	cont, err := endure.NewContainer(nil, endure.SetLogLevel(endure.ErrorLevel))
+	assert.NoError(t, err)
+
+	cfg := &config.Viper{
+		Path:   "configs/.rr-ssl-no-http.yaml",
+		Prefix: "rr",
+	}
+
+	err = cont.RegisterAll(
+		cfg,
+		&rpcPlugin.Plugin{},
+		&logger.ZapLogger{},
+		&server.Plugin{},
+		&httpPlugin.Plugin{},
+	)
+	assert.NoError(t, err)
+
+	err = cont.Init()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ch, err := cont.Serve()
+	assert.NoError(t, err)
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	stopCh := make(chan struct{}, 1)
+
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case e := <-ch:
+				assert.Fail(t, "error", e.Error.Error())
+				err = cont.Stop()
+				if err != nil {
+					assert.FailNow(t, "error", err.Error())
+				}
+			case <-sig:
+				err = cont.Stop()
+				if err != nil {
+					assert.FailNow(t, "error", err.Error())
+				}
+				return
+			case <-stopCh:
+				// timeout
+				err = cont.Stop()
+				if err != nil {
+					assert.FailNow(t, "error", err.Error())
+				}
+				return
+			}
+		}
+	}()
+
+	time.Sleep(time.Second * 1)
+	t.Run("SSLEcho", sslEcho)
+
+	stopCh <- struct{}{}
+	wg.Wait()
+	time.Sleep(time.Second)
+}
